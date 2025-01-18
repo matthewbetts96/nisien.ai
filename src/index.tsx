ff8ad198c8d1ae2c1f2e9dd58a72ad19e34f5d90
matthew.betts.96@gmail.com <matthew.betts.96@gmail.com>
2025-01-18 10:36:27 +0000

    Add roberto font

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,10 @@ import { LanguageProvider } from "./hooks/useLanguage/useLanguage";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient();
 
